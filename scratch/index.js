e378b43c8165614cef91cf5af9631043f510ef5c
manotasCoder <ruben.manotas.lopez@gmail.com>
2021-11-16 18:24:21 +0100

    section 4

--- scratch/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-// const element = React.createElement('div', null, 'react element');
-
-const element = <div>React is cool!</div>;
-
-ReactDOM.render(element, document.getElementById('root') );
